3c662fe7c5ac7fcb99135761bb3706b5aef3fc46
KotaruS <msoukup34@gmail.com>
2018-02-08 19:59:23 +0100

    edit 8/2

--- vypis.php
+++ vypis.php
@@ -13,18 +13,60 @@ $numba = 0;
       $stmt->execute();
       $pokemons = $stmt->fetchAll(PDO::FETCH_ASSOC);
     //  var_dump($pokemons);
+      $sql3 = 'SELECT * FROM typ;';
+      $stmt3 = $db->prepare($sql3);
+      $stmt3->execute();
+      $types_filter = $stmt3->fetchAll(PDO::FETCH_ASSOC);
 
-
-
+      $sql4 = 'SELECT * FROM clovek;';
+      $stmt4 = $db->prepare($sql4);
+      $stmt4->execute();
+      $humans_filter = $stmt4->fetchAll(PDO::FETCH_ASSOC);
   ?>
 </head>
 <body>
 <div class="container">
 
-
+<div class="row bg-primary">
+  <form method="get" class="col-12">
+    <input type="search" name="search"><button type="submit" name="submit">Bitch</button>
+  </form>
+  <div class="row m-0">
+  <form method="get" class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 ">
+    <?php foreach ($types_filter as $type_filter)
+     {
+     if(($type_filter['id']-1) % 6 == 0  /*&& count($pokemons) - ($numba+4) !== 0*/) { echo '<div class="row">';}
+    ?>
+      <div class="col-6 col-sm-6 col-md-4 col-lg-2 col-xl-2 mx-0 my-1">
+       <label class="typ_nazev col-12 py-2 py-sm-2 py-md-1 bgcolor-<?php echo strtolower($type_filter['nazev_typu']);?>" ><?php echo $type_filter['nazev_typu']; ?>
+       <input type="checkbox" name="typ[]" value="<?php echo $type_filter['id']; ?>">
+       <span class="checkmark bgcolor-<?php echo strtolower($type_filter['nazev_typu']);?>"></span>
+       </label>
+      </div>
+     <?php
+     if($type_filter['id'] % 6 == 0) {echo "</div>";}
+     }
+     ?>
+   </form>
+   <div class="col-12 col-sm-12 col-md-12 col-lg-2 col-xl-2 m-0">
+     <?php foreach ($humans_filter as $human_filter)
+      {
+    //  if(($human_filter['id']-1) % 6 == 0  /*&& count($pokemons) - ($numba+4) !== 0*/) { echo '<div class="row">';}
+     ?>
+        <label class="typ_nazev col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 badge-info" ><?php echo $human_filter['jmeno']; ?>
+        <input type="checkbox" name="typ[]" value="<?php echo $human_filter['id']; ?>">
+        <span class="checkmark"></span>
+        </label>
+      <?php
+    //  if($human_filter['id'] % 6 == 0) {echo "</div>";}
+      }
+      ?>
+    </div>
+  </div>
+</div>
 
 <?php
-// vypis v kartickach
+// vypis
 foreach ($pokemons as $pokemon) {
 
 if(($pokemon['id']-1) % 4 == 0  /*&& count($pokemons) - ($numba+4) !== 0*/) {
@@ -48,11 +90,11 @@ if(($pokemon['id']-1) % 4 == 0  /*&& count($pokemons) - ($numba+4) !== 0*/) {
   $sql2 = 'SELECT nazev_typu FROM `pokemon_typ` JOIN typ ON pokemon_typ.typ_id = typ.id WHERE pokemon_id = :id;';
   $stmt2 = $db->prepare($sql2);
   $stmt2->execute([':id' => $pokemon['id']]);
-  $type = $stmt2->fetchAll(PDO::FETCH_ASSOC);
-  foreach ($type as $onetype) {
-    //echo $onetype['nazev_typu'];
-    //var_dump($onetype);
-    echo "<div class='col-3 col-sm-4 col-md-4 col-lg-4 col-xl-4 ml-2  text-center p-0 rounded bgcolor-" . strtolower($onetype['nazev_typu']) . "'>" . $onetype['nazev_typu'] . "</div>" ;
+  $types = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($types as $type) {
+    //echo $type['nazev_typu'];
+    //var_dump($type);
+    echo "<div class='col-3 col-sm-4 col-md-4 col-lg-4 col-xl-4 ml-2  text-center p-0 rounded bgcolor-" . strtolower($type['nazev_typu']) . "'>" . $type['nazev_typu'] . "</div>" ;
   }
 //  echo "";
   echo "</div></div></div></div>";
