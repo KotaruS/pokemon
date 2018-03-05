a16b6d15cc1c6bd50ba2fb98a564264915ba91b9
KotaruS <msoukup34@gmail.com>
2018-03-05 08:38:02 +0100

    ....e

--- handler.php
+++ handler.php
@@ -10,11 +10,5 @@ switch ($current) {
     setcookie('theme', 'dark', time() + (86400 * 365 * 3), "/");
 
 }
-if (isset($_GET['theme'])) {
-  header('Location:' . $_SERVER['HTTP_REFERER']);
-}
-if (isset($_GET['limitoff'])) {
-  header('Location:' . $_SERVER['HTTP_REFERER'] . '&limitoff=' . $_GET['limitoff']);
-}
-
+header('Location:' . $_SERVER['HTTP_REFERER']);
 ?>
